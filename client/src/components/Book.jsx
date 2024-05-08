b70d7bbefb135fc308b94331a20ad143cf22d9b6
RonanCraft <SuperRonanCraft@gmail.com>
2024-05-08 10:14:43 -0400

    delete book fix

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -1,9 +1,14 @@
 import { Col, Button, Card } from 'react-bootstrap';
 import Auth from '../utils/auth';
 
-export default function Book({ book, savedBookIds, handleSaveBook }) {
+export default function Book({
+  book,
+  savedBookIds,
+  handleSaveBook,
+  handleDeleteBook,
+}) {
   return (
-    <Col md="4" key={book.bookId} className="my-2">
+    <Col md="3" key={book.bookId} className="my-2">
       <Card border="dark">
         {book.image ? (
           <Card.Img
@@ -18,17 +23,28 @@ export default function Book({ book, savedBookIds, handleSaveBook }) {
           <Card.Text>
             {book.description ? trimDesc(book.description) : ''}
           </Card.Text>
-          {Auth.loggedIn() && (
+          {handleSaveBook ? (
+            Auth.loggedIn() && (
+              <Button
+                disabled={savedBookIds?.some(
+                  (savedBookId) => savedBookId === book.bookId
+                )}
+                className="btn-block btn-info"
+                onClick={() => handleSaveBook(book.bookId)}
+              >
+                {savedBookIds?.some(
+                  (savedBookId) => savedBookId === book.bookId
+                )
+                  ? 'This book has been saved!'
+                  : 'Save this Book!'}
+              </Button>
+            )
+          ) : (
             <Button
-              disabled={savedBookIds?.some(
-                (savedBookId) => savedBookId === book.bookId
-              )}
-              className="btn-block btn-info"
-              onClick={() => handleSaveBook(book.bookId)}
+              className="btn-block btn-danger"
+              onClick={() => handleDeleteBook(book.bookId)}
             >
-              {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                ? 'This book has already been saved!'
-                : 'Save this Book!'}
+              Delete this Book!
             </Button>
           )}
         </Card.Body>
--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,15 +1,10 @@
 import { useState, useEffect } from 'react';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
+import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import Book from '../components/Book';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
@@ -79,27 +74,21 @@ const SavedBooks = () => {
         </Container>
       </div>
       <Container>
-        <h2 className='pt-5'>
+        <h2 className="pt-5">
           {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? 'book' : 'books'
+              }:`
             : 'You have no saved books!'}
         </h2>
         <Row>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks.map((book, index) => {
             return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                      Delete this Book!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
+              <Book
+                key={index}
+                book={book}
+                handleDeleteBook={handleDeleteBook}
+              />
             );
           })}
         </Row>
